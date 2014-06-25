fb8099897bab811c7d341da1335db4a23e09471f
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-06-25 08:05:17 +0200

    dev+

---
+++ include/API/marpaXml/StaticClassPrototype.h
@@ -0,0 +1,14 @@
+#ifndef MARPAXML_STATICCLASSPROTOTYPE
+#define MARPAXML_STATICCLASSPROTOTYPE
+
+/*
+  We are not gonna use a generic C object simulation framework because
+  our class are all static.
+  Excellent literature on this subject can found at e.g. http://ldeniau.web.cern.ch/ldeniau/oopc.html
+  and elsewhere on the internet.
+
+  Because everything in our case is static we handle ourself class and objects with our own
+  macros
+*/
+
+#endif /* MARPAXML_STATICCLASSPROTOTYPE */
