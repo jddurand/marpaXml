637233cc2a41f613d48cb8202b1351ebe617bb8a
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-06-25 08:28:45 +0200

    dev+

--- include/API/marpaXml/StaticClassPrototype.h
+++ include/API/marpaXml/StaticClassPrototype.h
@@ -8,7 +8,15 @@
   and elsewhere on the internet.
 
   Because everything in our case is static we handle ourself class and objects with our own
-  macros
+  macros. This implementation only support inheritance. Which is enough in case of DOM3 and SAX2.
 */
+
+#define module(name) MARPAXML ## _ ## name
+
+#define class_decl(class) typedef struct class *class;
+#define class_meth(class, meth) class ## _ ## method
+
+#define subclass_decl(subclass, class) typedef struct subclass { class *parent; } *subclass;
+#define subclass_meth(subclass, class, meth) subclass ## _ ## meth
 
 #endif /* MARPAXML_STATICCLASSPROTOTYPE */
