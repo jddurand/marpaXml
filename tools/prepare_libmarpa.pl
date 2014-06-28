273d2c38cd5dfd76f41bb40f284a7b34d2928311
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-06-28 18:14:12 +0200

    tools/prepare_libmarpa.pl: build also classical Marpa::R2

--- tools/prepare_libmarpa.pl
+++ tools/prepare_libmarpa.pl
@@ -14,8 +14,27 @@ my $dir_dist = catdir($dir, 'libmarpa_dist');
 
 rmtree($dir);
 system($^X, catfile('tools', 'extract.pl'), catfile('tar', "$dir.tar.gz"));
-copy(catfile($dir, 'libmarpa_dist', 'LIB_VERSION'), catfile($dir, 'libmarpa_dist', 'VERSION'));
+#
+# Specific to Marpa version <= 2.085_006
+#
+my $LIB_VERSION = catfile($dir, 'libmarpa_dist', 'LIB_VERSION');
+my $VERSION = catfile($dir, 'libmarpa_dist', 'VERSION');
+if (-e $LIB_VERSION && ! -e $VERSION) {
+    copy($LIB_VERSION, $VERSION);
+}
 chdir($dir_dist) || die "Cannot chdir to $dir_dist, $!";
+print STDERR "\n";
+print STDERR "*** Configuring in $dir_dist\n";
+print STDERR "\n";
 system($^X, catfile('win32', 'do_config_h.pl'), '--use_perl_autoconf', '--cc', $CC_BASENAME, '--ccflags', $CMAKE_C_FLAGS, '--obj_ext', $CMAKE_C_OUTPUT_EXTENSION);
+chdir(updir) || die "Cannot chdir to " . updir . ", $!";
+print STDERR "\n";
+print STDERR "*** Configuring in $dir\n";
+print STDERR "\n";
+system($^X, 'Build.PL');
+print STDERR "\n";
+print STDERR "*** Building in $dir\n";
+print STDERR "\n";
+system(catfile(curdir, 'Build'));
 
 exit(EXIT_SUCCESS);
