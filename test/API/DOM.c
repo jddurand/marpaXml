12d242db514400ce8817c079f3c7be25d7980347
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-08-06 20:43:52 +0200

    use C only even for UChar *constants

--- test/API/DOM.c
+++ test/API/DOM.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include "API/marpaXml/DOM.h"
-#include "internal/DOMStringLiterals.hpp"
 #include <unicode/ustring.h>
 
+U_STRING_DECL(MARPAXML_DOM_STRINGLITERAL_CORE, "Core", 4);
+U_STRING_DECL(MARPAXML_DOM_STRINGLITERAL_XML, "XML", 3);
+U_STRING_DECL(DOMStringUnknown, "Unknown", 7);
+
 int main(int argc, char **argv) {
-  MARPAXML_DOM_STRUCT(DOMString)       DOMString;
-  U_STRING_DECL(DOMStringUnknown, "Unknown", 7);
+  U_STRING_INIT(                 MARPAXML_DOM_STRINGLITERAL_CORE, "Core", 4);
+  U_STRING_INIT(                 MARPAXML_DOM_STRINGLITERAL_XML, "XML", 3);
+  U_STRING_INIT(                 DOMStringUnknown, "Unknown", 7);
+  MARPAXML_DOM_STRUCT(DOMString) DOMString;
 
   MARPAXML_DOM_TYPE(DOMImplementation) DOMImplementationp = MARPAXML_DOM_NEW(DOMImplementation)();
 
-  DOMString.s = (UChar *) marpaXml_DOMStringLiteral_buffer(MARPAXML_DOM_STRINGLITERAL_CORE);
-  DOMString.length = marpaXml_DOMStringLiteral_length(MARPAXML_DOM_STRINGLITERAL_CORE);
+  DOMString.s = (unsigned short *) MARPAXML_DOM_STRINGLITERAL_CORE;
+  DOMString.length = 4;
   fprintf(stderr, "DOMImplementationp->hasFeature(\"Core\", NULL): %s\n", DOMImplementationp->hasFeature(&DOMString, NULL) == MARPAXML_DOM_TRUE ? "OK" : "KO");
 
-  DOMString.s = (UChar *) marpaXml_DOMStringLiteral_buffer(MARPAXML_DOM_STRINGLITERAL_XML);
-  DOMString.length = marpaXml_DOMStringLiteral_length(MARPAXML_DOM_STRINGLITERAL_XML);
+  DOMString.s = (unsigned short *) MARPAXML_DOM_STRINGLITERAL_XML;
+  DOMString.length = 3;
   fprintf(stderr, "DOMImplementationp->hasFeature(\"XML\", NULL): %s\n", DOMImplementationp->hasFeature(&DOMString, NULL) == MARPAXML_DOM_TRUE ? "OK" : "KO");
 
-  U_STRING_INIT(DOMStringUnknown, "Unknown", 7);
-  DOMString.s = DOMStringUnknown;
-  DOMString.length = u_strlen(DOMStringUnknown);
+  DOMString.s = (unsigned short *) DOMStringUnknown;
+  DOMString.length = 7;
   fprintf(stderr, "DOMImplementationp->hasFeature(\"Unknown\", NULL): %s\n", DOMImplementationp->hasFeature(&DOMString, NULL) == MARPAXML_DOM_FALSE ? "OK" : "KO");
 
   MARPAXML_DOM_FREE(DOMImplementation)(DOMImplementationp);
