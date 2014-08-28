95eaca0add5e14cb5ae684b1d107a9520dd54366
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-08-28 07:55:38 +0200

    test/API/String_test.c: Better string test

--- test/API/String_test.c
+++ test/API/String_test.c
@@ -4,26 +4,73 @@
  
 #include "API/marpaXml/String.h"
 
-#define TESTSTRING (char *) "TestString"
+const char *testUtf8String = "myString";
+const char *testGb2312String = "\xbe\xb2\xcc\xac\xc4\xa3\xca\xbd"; /* http://www.cprogramdevelop.com/1747404/ */
 
 void hexdump(void *mem, unsigned int len);
+void test(char *bytes, size_t byteLength, char *charset, marpaXml_String_Option_t *optionp);
 
 struct marpaXml_String_Option option = {
   MARPAXML_STRING_OPTION_ICU_DEFAULT,
   marpaXml_false,
   {NULL,
    NULL,
-   MARPAXML_LOGLEVEL_TRACE
+   MARPAXML_LOGLEVEL_INFO
   }
 };
 
-int main(int argc, char **argv) {
+void test(char *origBytes, size_t origByteLength, char *origCharset, marpaXml_String_Option_t *optionp) {
+  char              *p;
+  size_t             byteLength;
+  size_t             length;
+  marpaXml_String_t *s;
 
-  marpaXml_String_t *s = marpaXml_String_newFromAnyAndByteLengthAndCharset(TESTSTRING, strlen(TESTSTRING), NULL, &option);  /* Null byte volontarily ommited */
+  /* First with the null byte added */
+  s = marpaXml_String_newFromAnyAndByteLengthAndCharset(origBytes, origByteLength, origCharset, &option);  /* Null byte volontarily ommited */
 
-  fprintf(stdout, "\nUTF-8   : %d characters, %d bytes, null byte added: %s\n", marpaXml_String_getLength(s), marpaXml_String_getUtf8ByteLength(s), (marpaXml_String_getNullByteAddedb(s) == marpaXml_true) ? "yes" : "no");
+  fprintf(stdout, "\n==>UTF-8 : %ld characters, %ld bytes, null byte added: %s\n", (unsigned long) marpaXml_String_getLength(s), (unsigned long) marpaXml_String_getUtf8ByteLength(s), (marpaXml_String_getNullByteAddedb(s) == marpaXml_true) ? "yes" : "no");
+  hexdump(marpaXml_String_getUtf8(s), marpaXml_String_getUtf8ByteLength(s));
+
+  p = marpaXml_String_encode(s, &byteLength, &length, (char *) "UTF-8", &option);
+  fprintf(stdout, "\n-->UTF-8  : %ld characters, %ld bytes\n", (unsigned long) length, (unsigned long) byteLength);
+  hexdump(p, byteLength);
+
+  p = marpaXml_String_encode(s, &byteLength, &length, (char *) "UCS-2", &option);
+  fprintf(stdout, "\n-->UCS-2  : %ld characters, %ld bytes\n", (unsigned long) length, (unsigned long) byteLength);
+  hexdump(p, byteLength);
+
+  p = marpaXml_String_encode(s, &byteLength, &length, (char *) "UTF-32", &option);
+  fprintf(stdout, "\n-->UTF-32  : %ld characters, %ld bytes\n", (unsigned long) length, (unsigned long) byteLength);
+  hexdump(p, byteLength);
+
+  p = marpaXml_String_encode(s, &byteLength, &length, (char *) "GB2312", &option);
+  fprintf(stdout, "\n-->GB2312  : %ld characters, %ld bytes\n", (unsigned long) length, (unsigned long) byteLength);
+  hexdump(p, byteLength);
 
   marpaXml_String_free(&s);
+}
+
+int main(int argc, char **argv) {
+
+  /* First with the null byte added, then without */
+
+  fprintf(stdout, "*** Origin: UTF-8 string with no charset hint\n");
+  fprintf(stdout, "*********************************************\n");
+  test((char*) testUtf8String, strlen(testUtf8String)    , NULL, &option);
+  test((char *)testUtf8String, strlen(testUtf8String) + 1, NULL, &option);
+
+  fprintf(stdout, "*** Origin: UTF-8 string with charset hint\n");
+  fprintf(stdout, "******************************************\n");
+  test((char*) testUtf8String, strlen(testUtf8String)    , (char *) "UTF-8", &option);
+  test((char *)testUtf8String, strlen(testUtf8String) + 1, (char *) "UTF-8", &option);
+
+  fprintf(stdout, "*** Origin: GB2312 string with no charset hint\n");
+  fprintf(stdout, "**********************************************\n");
+  test((char*) testGb2312String, strlen(testGb2312String), NULL, &option);
+
+  fprintf(stdout, "*** Origin: GB2312 string with charset hint\n");
+  fprintf(stdout, "*******************************************\n");
+  test((char*) testGb2312String, strlen(testGb2312String), (char*) "GB2312", &option);
 
   return 0;
 }
