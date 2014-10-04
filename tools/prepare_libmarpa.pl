87486d335d9e956c94813d9be24d3c49eee7d549
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-10-04 18:38:40 +0200

    Marpa-R2-2.097_001

--- tools/prepare_libmarpa.pl
+++ tools/prepare_libmarpa.pl
@@ -1,7 +1,7 @@
 #!env perl
 use strict;
 use diagnostics;
-use POSIX qw/EXIT_SUCCESS/;
+use POSIX qw/EXIT_SUCCESS EXIT_FAILURE/;
 use File::Copy;
 use File::Path;
 use ExtUtils::Command qw/rm_rf cp/;
@@ -10,13 +10,21 @@ use File::Spec::Functions;
 my ($MARPA_VERSION, $CC_BASENAME, $CMAKE_C_FLAGS, $CMAKE_C_OUTPUT_EXTENSION) = @ARGV;
 
 my $dir = "Marpa-R2-${MARPA_VERSION}";
-my $dir_dist = catdir($dir, 'core', 'read_only');
+my @dir_dist = (
+    catdir($dir, 'core', 'read_only'),
+    catdir($dir, 'engine', 'read_only')
+    );
 
 rmtree($dir);
 system($^X, catfile('tools', 'extract.pl'), catfile('tar', "$dir.tar.gz"));
 #
 # Specific to Marpa version <= 2.085_006
 #
+my $dir_dist = (grep {-d $_} @dir_dist)[0];
+if (! $dir_dist) {
+    print STDERR "Cannot find libmarpa directory (tried @dir_dist)\n";
+    exit(EXIT_FAILURE);
+}
 my $LIB_VERSION = catfile($dir_dist, 'LIB_VERSION');
 my $VERSION = catfile($dir_dist, 'VERSION');
 if (-e $LIB_VERSION && ! -e $VERSION) {
